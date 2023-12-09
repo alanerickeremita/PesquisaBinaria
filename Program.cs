c27ee7734969094703f670feaf564385b8c6dfee
Alan Erick Eremita Dias <alanerickeremita@gmail.com>
2023-12-09 15:06:23 -0300

    feat: adição de validação de valor não existente no array.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@
 
       Console.WriteLine("Digite um valor: ");
       var valor = Console.ReadLine();
-      Console.WriteLine(@"A posição do valor informado no array: { 1, 3, 5, 7, 9, 11 } é: " + PesquisaBinaria(lista, Convert.ToInt32(valor)));
+      var retornoPesquisa = PesquisaBinaria(lista, Convert.ToInt32(valor));
+
+      if (retornoPesquisa.HasValue)
+        Console.WriteLine(@"A posição do valor informado no array: { 1, 3, 5, 7, 9, 11 } é: " + retornoPesquisa);
+      else
+        Console.WriteLine(@"O valor informado: " + valor + " ,não existe no array: { 1, 3, 5, 7, 9, 11 }");
     }
 
     private static int? PesquisaBinaria(int[] lista, int valor)
@@ -16,7 +21,7 @@
       var minimo = 0;
       var maximo = lista.Length;
 
-      while (minimo <= maximo)
+      while (minimo < maximo)
       {
         var meio = (maximo + minimo) / 2;
         var chute = lista[meio];
